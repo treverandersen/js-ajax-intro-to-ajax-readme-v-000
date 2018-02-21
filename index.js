3fe835defed7fef32688e9f2e28fbed22e561170
Trever Andersen <trever.r.andersen@gmail.com>
2018-02-21 23:11:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function showRepositories(event, data) {
   console.log(this.responseText);
+  let repoList = "<ul>";
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
+  }
+  repoList += "<ul>"
+  document.getElementById("repositories").innerHTML = repoList
 }
 
 function getRepositories() {
