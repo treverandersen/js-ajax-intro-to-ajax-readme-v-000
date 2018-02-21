194268c64dcc2e460243042d9f6ec9c3ab44d113
Trever Andersen <trever.r.andersen@gmail.com>
2018-02-21 23:07:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function getRepositories() {
   const req = new XMLHttpRequest();
+  req.addEventListener("load", showRepositories);
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send()
+}
+
+function showRepositories(event, data) {
+  console.log(this.responseText);
 }
