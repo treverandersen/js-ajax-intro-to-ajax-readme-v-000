1c3e119e6982f8486e018285aad41fb18e0068c3
Trever Andersen <trever.r.andersen@gmail.com>
2018-02-21 23:15:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 function showRepositories(event, data) {
-  console.log(this.responseText);
-  let repoList = "<ul>";
-  for (var i = 0; i < this.responseText.length; i++) {
-    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
-  }
-  repoList += "<ul>"
-  document.getElementById("repositories").innerHTML = repoList
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join(' ')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList;
 }
 
 function getRepositories() {
